8d6a18e64a8bc2ddafd99fa0e61ed882c6ebd83b
Denys Martsyn <aqezy21@gmail.com>
2025-03-19 19:58:24 +0200

    feat: implement role-based system on endpoints

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -10,31 +10,38 @@ import {
 import { GroupsService } from './groups.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
+import { Role } from 'src/users/role.enum';
+import { Roles } from 'src/auth/decorators';
 
 @Controller('groups')
 export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
+  @Roles(Role.Editor)
   @Post()
   create(@Body() createGroupDto: CreateGroupDto) {
     return this.groupsService.create(createGroupDto);
   }
 
+  @Roles(Role.Viewer, Role.Editor)
   @Get()
   findAll() {
     return this.groupsService.findAll();
   }
 
+  @Roles(Role.Viewer, Role.Editor)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.groupsService.findOne(id);
   }
 
+  @Roles(Role.Editor)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
     return this.groupsService.update(id, updateGroupDto);
   }
 
+  @Roles(Role.Editor)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.groupsService.remove(id);
--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -9,31 +9,38 @@ import {
 } from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { CreateVideoDto, UpdateVideoDto } from './dto';
+import { Roles } from 'src/auth/decorators';
+import { Role } from 'src/users/role.enum';
 
 @Controller('videos')
 export class VideosController {
   constructor(private readonly videosService: VideosService) {}
 
+  @Roles(Role.Editor)
   @Post()
   create(@Body() createVideoDto: CreateVideoDto) {
     return this.videosService.create(createVideoDto);
   }
 
+  @Roles(Role.Viewer, Role.Editor)
   @Get()
   findAll() {
     return this.videosService.findAll();
   }
 
+  @Roles(Role.Viewer, Role.Editor)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.videosService.findOne(id);
   }
 
+  @Roles(Role.Editor)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateVideoDto: UpdateVideoDto) {
     return this.videosService.update(id, updateVideoDto);
   }
 
+  @Roles(Role.Editor)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.videosService.remove(id);
